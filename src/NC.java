ebc5d277ad356dfc1929166a1275407647eddab6
Nico Cortez <nicolas.cortez@live.u-tad.com>
2024-11-25 22:46:26 +0100

    Modificación 1

--- src/NC.java
+++ src/NC.java
@@ -2,5 +2,6 @@ public class NC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alumno: Nicolas Cortez Gomez");
+		System.out.println("Primera Modificación desde Master");
 	}
-}
+}	
